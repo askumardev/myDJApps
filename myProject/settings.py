ff34ab2f3811a71b21da4ad14d3c854e8bb5ed38
askumardev <askumar.dev@outlook.com>
2024-06-28 18:21:22 +0530

    sample content in static folders

--- myProject/settings.py
+++ myProject/settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 # IMAGE_URL = 'images/'
 STATICFILE_DIR = (
-    os.path.join(BASE_DIR,'static')
+    os.path.join(BASE_DIR, 'static'),
 )
 
 # Default primary key field type
