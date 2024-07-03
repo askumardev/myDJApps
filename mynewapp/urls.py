76c497da72d8543438ddd06f85fc853f1d3e57bd
askumardev <askumar.dev@outlook.com>
2024-07-03 19:23:12 +0530

    books index with model

--- mynewapp/urls.py
+++ mynewapp/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('newpage/', views.new_page, name='new_page'),
+    path('books/', views.books_index, name='books_index'),
+    path('books/add/', views.add_book, name='add_book'),
 ]
--- mynewapp/views.py
+++ mynewapp/views.py
@@ -1,5 +1,22 @@
 # mynewapp/views.py
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Book
+from .forms import BookForm
 
 def new_page(request):
     return render(request, 'mynewapp/new_page.html')
+
+
+def books_index(request):
+    books = Book.objects.all()
+    return render(request, 'mynewapp/books_index.html', {'books': books})
+
+def add_book(request):
+    if request.method == 'POST':
+        form = BookForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('books_index')
+    else:
+        form = BookForm()
+    return render(request, 'mynewapp/add_book.html', {'form': form})
